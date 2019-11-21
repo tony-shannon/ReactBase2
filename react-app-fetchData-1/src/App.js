35d44da49914e70599d6a5ad46cebeb4816cd328
Tony Shannon <tony.shannon@ripple.foundation>
2019-11-21 14:35:54 +0000

    setup stripped Table - fetch user from API

---
+++ react-app-fetchData-1/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+import UserTableReactHooks from "./components/UserTableReactHooks";
+
+//import SimpleUserTable from "./components/SimpleUserTable";
+
+
+function App() {
+    return (
+        <div className='App'>
+                         
+            <h2> User Table - React Hooks</h2>
+            <UserTableReactHooks/>
+        </div>
+    );
+}
+
+export default App 
---
+++ react-app-fetchData-1/src/components/UserTableReactHooks.js
@@ -0,0 +1,30 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import SimpleUserTable from "./SimpleUserTable";
+
+const USER_SERVICE_URL = 'https://jsonplaceholder.typicode.com/users';
+
+
+function UserTableReactHooks() {
+    const [data, setData] = useState({users: [], isFetching: false});
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                setData({users: data.users, isFetching: true});
+                const response = await axios.get(USER_SERVICE_URL);
+                setData({users: response.data, isFetching: false});
+            } catch (e) {
+                console.log(e);
+                setData({users: data.users, isFetching: false});
+            }
+        };
+        fetchUsers();
+    }, []);
+
+    return <SimpleUserTable data={data.users}
+                            isFetching={data.isFetching}
+    />
+}
+
+export default UserTableReactHooks
