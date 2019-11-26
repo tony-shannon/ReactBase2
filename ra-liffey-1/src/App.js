2d54d4d5c57350b0a248e80dfdfe9bec3ceecd35
Tony Shannon <tony.shannon@ripple.foundation>
2019-11-26 17:54:38 +0000

    added JSON Data Provider -compiled from TS and working

--- ra-liffey-1/src/App.js
+++ ra-liffey-1/src/App.js
@@ -1,14 +1,15 @@
 // in src/App.js
 import React from 'react';
 import { Admin, Resource, ListGuesser, ShowGuesser } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
-
+//import jsonServerProvider from 'ra-data-json-server';
+import jsonServerProvider from './components/JSONDP_fromTS.js';
 import UserTableReactHooks from "./components/UserTableReactHooks";
 
 
 //const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 //const dataProvider = jsonServerProvider('https://8t4vl.sse.codesandbox.io');
-const dataProvider = jsonServerProvider('https://5000-d0316dbb-8407-4a0b-a77d-da608a2545f3.ws-eu01.gitpod.io');
+//const dataProvider = jsonServerProvider('https://5000-d0316dbb-8407-4a0b-a77d-da608a2545f3.ws-eu01.gitpod.io');
+const dataProvider = jsonServerProvider('https://5000-fd3a8674-79e0-450e-b2cb-a3692ba982a9.ws-eu01.gitpod.io');
 const App = () => (
     <div>
     <Admin 
