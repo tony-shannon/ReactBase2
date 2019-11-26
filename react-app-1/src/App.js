59dd9be42a4796170c79f23f4003d36b9930ea8a
Tony Shannon <tony.shannon@ripple.foundation>
2019-11-26 15:35:16 +0000

    added in TS module for JSDP -compiled it & used it

--- react-app-1/src/App.js
+++ react-app-1/src/App.js
@@ -2,7 +2,9 @@
 import React from 'react';
 //-import { Admin } from 'react-admin';
 import { Admin, Resource, ListGuesser } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
+//import jsonServerProvider from 'ra-data-json-server';
+//import jsonServerProvider from './JSONProviderC.js';
+import jsonServerProvider from './components/JSON_fromTS/index.js';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 //const App = () => <Admin dataProvider={dataProvider} />;
